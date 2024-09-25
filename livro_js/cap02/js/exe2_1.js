dfaf00cf806ad9b1225a9340c83f9ca89a10bc3f
ead <ead@academico.local>
2024-09-24 21:50:24 -0300

    aula_html

---
+++ livro_js/cap02/js/exe2_1.js
@@ -0,0 +1,12 @@
+// criar uma referência ao form e ao h3(onde será exibida a resposta)
+
+const frm = document.querySelector("form");
+
+const resp = document.querySelector("h3");
+// cria um "ouvinte" de evento, acionado quando o botão submit for clicado 
+
+frm.addEventListener("submit", (e) => {
+    const nome = frm.inNome.value; //obtém o nome digitado no form
+     resp.innerText = `Olá ${nome}`; // exibe a resposta do programa
+     e.preventDefault(); // vai evitar que o form envie os dados pro destino.php
+});
